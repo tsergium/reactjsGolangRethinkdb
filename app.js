192eb7af1404ffe760d2879db492fe831ca2a1a2
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-02-21 11:14:55 +0100

    React prototype

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sergiu on 21.02.17.
+ */
+class Channel extends React.Component{
+    render(){
+        return (
+            <li>Channel name</li>
+        )
+    }
+}
+
+ReactDOM.render(<Channel/>, document.getElementById('app'));
