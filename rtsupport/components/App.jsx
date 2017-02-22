c02fd9deb35c4a793969218d2ab222f1ce2fab1b
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2017-02-22 16:45:08 +0100

    React prototype and websockets

---
+++ rtsupport/components/App.jsx
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import ChannelSection from './channels/ChannelSection.jsx';
+import UserSection from './users/UserSection.jsx';
+import MessageSection from './messages/MessageSection.jsx';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            channels: [],
+            users: [],
+            messages: [],
+            activeChannel: {},
+            connected: false
+        };
+    }
+
+    componentDidMount(){
+        let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+        ws.onmessage = this.message.bind(this);
+        ws.onopen = this.open.bind(this);
+        ws.onclose = this.close.bind(this);
+    }
+
+    message(e){
+        const event = JSON.parse(e.data);
+        if(event.name === 'channel add'){
+            this.newChannel(event.data);
+        }
+    }
+
+    open(){
+        this.setState({connected: true});
+    }
+
+    close(){
+        this.setState({connected: false});
+    }
+
+    newChannel(channel){
+        let {channels} = this.state;
+        channels.push(channel);
+        this.setState({channels});
+    }
+
+    addChannel(name){
+        let {channels} = this.state;
+
+        // ToDo: Send to server
+        let msg = {
+            name: 'channel add',
+            data: {
+                id: channels.length,
+                name
+            }
+        }
+        this.ws.send(JSON.stringify(msg))
+    }
+
+    setChannel(activeChannel){
+        this.setState({activeChannel});
+        // ToDo: Get Channels Messages
+    }
+
+    setUserName(name){
+        let {users} = this.state;
+        users.push({id: users.length, name});
+        this.setState({users});
+        // ToDo: Send to server
+    }
+
+    addMessage(body){
+        let {messages, users} = this.state;
+        let createdAt = new Date;
+        let author = users.length > 0 ? users[0].name : 'anonymous';
+        messages.push({id: messages.length, body, createdAt, author});
+        this.setState({messages});
+        // ToDo: Send to server
+    }
+
+    render(){
+        return (
+            <div className="app">
+                <div className="nav">
+                    <ChannelSection
+                        {...this.state}
+                        addChannel={this.addChannel.bind(this)}
+                        setChannel={this.setChannel.bind(this)}
+                    />
+                    <UserSection
+                        {...this.state}
+                        setUserName={this.setUserName.bind(this)}
+                    />
+                </div>
+                <MessageSection
+                    {...this.state}
+                    addMessage={this.addMessage.bind(this)}
+                />
+            </div>
+        )
+    }
+}
+
+export default App
